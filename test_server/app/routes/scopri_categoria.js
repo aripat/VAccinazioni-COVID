5e13acc164cf4cf938b9083ec1a25c513cad707f
elenasofia98 <elenasofia98@gmail.com>
2021-06-15 15:40:06 +0200

    server update

--- test_server/app/routes/scopri_categoria.js
+++ test_server/app/routes/scopri_categoria.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/poli_vaccinali.html', function(req, res, next) {
+  res.render('poli_vaccinali', { title: 'Home' });
 });
 
 module.exports = router;
