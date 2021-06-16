50ad0ec81a6d4d018d5f04f87354521bb8e88b47
elenasofia98 <elenasofia98@gmail.com>
2021-06-16 10:08:12 +0200

    server update

--- test_server/app/connection/makeconnection.js
+++ test_server/app/connection/makeconnection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-  host     : '127.0.0.1',
+  host     : '10.0.2.15',
   user     : 'portale_prenotazioni',
   password : 'portale_prenotazioni',
   database : 'prenotazioni_vaccinazioni'
