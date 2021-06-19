20df8d89080696dd1718cd69f0372307c8d798ec
elenasofia98 <elenasofia98@gmail.com>
2021-06-19 15:15:10 +0200

    server update

--- test_server/app/routes/scopri_categoria.js
+++ test_server/app/routes/scopri_categoria.js
@@ -3,17 +3,16 @@ var router = express.Router();
 var connection = require('../connection/makeconnection.js');
 
 /* GET users listing. */
+connection.connect(); //? forse errore
+
 router.get('/poli_vaccinali.html', function(req, res, next) {
   const categoria = req.query.categoria
-  //connection.connect(); ? forse errore
-  connection.query('SELECT * FROM polo_per_categoria WHERE categoria=\''+categoria+'\'', function (error, results, fields) {
+  connection.query('SELECT * FROM erogazione, polo_per_categoria WHERE erogazione.polo_vaccinale = polo_per_categoria.polo_vaccinale AND categoria=\''+categoria+'\' ORDER BY erogazione.data, erogazione.ora', function (error, results, fields) {
     // error will be an Error if one occurred during the query
     // results will contain the results of the query
     // fields will contain information about the returned results fields (if any)
-    
-    console.log(results);
+    res.render('poli_vaccinali', { title: 'Home', results: results });
   });
-  res.render('poli_vaccinali', { title: 'Home' });
 });
 
 module.exports = router;
