1346002deea1d1dc6d3c15dd76639768a8674216
elenasofia98 <elenasofia98@gmail.com>
2021-06-21 20:52:49 +0200

    update server

--- test_server/app/routes/prenota-appuntamento.js
+++ test_server/app/routes/prenota-appuntamento.js
@@ -3,12 +3,23 @@ var router = express.Router();
 var connection = require('../connection/makeconnection.js');
 
 router.post('/', function(req, res, next) {
-    //TODO INSERT
+    console.log(req.body)
+    const codice_fiscale = req.body.CF;
+    //const categoria = req.body.categoria;
+    const TEAM = req.body.TEAM;
+    //const polo_vaccinale= req.body.polo_vaccinale;
+    //const data = req.body.data;
+    //const ora = req.body.ora;
+    //const vaccino= req.body.vaccino;
+    const id_erogazione = req.body.id_erogazione
     //TODO encrypt per generare numero prenotazione a 14 cifre
-    connection.query('SELECT * FROM erogazione WHERE 1', function (error, results, fields) {
-        console.log(results)
-        
-        console.log("prenota appuntamto GET")
+    var numero_richiesta = '00000000000002'
+    //TODO fail per id_erogazione duplicata
+    
+    const sql = 'INSERT INTO `prenotazioni`(`codice_fiscale`, `team`, `numero_richiesta`, `id_erogazione`) VALUES (\''+codice_fiscale+'\',\''+ TEAM +'\', \''+ numero_richiesta +'\',\''+id_erogazione+'\')'
+
+    connection.query(sql, function (error, results, fields) {
+        console.log("prenota appuntamento GET")
         res.render('prenota-appuntamento', { title: 'Home' });
     });
 });
--- test_server/app/routes/scopri-categoria.js
+++ test_server/app/routes/scopri-categoria.js
@@ -2,17 +2,14 @@ var express = require('express');
 var router = express.Router();
 var connection = require('../connection/makeconnection.js');
 
-/* GET users listing. */
-
-router.get('/poli-vaccinali.html', function(req, res, next) {
+router.get('/poli-vaccinali', function(req, res, next) {
   const codice_fiscale = req.query.CF;
   const team = req.query.TEAM;
   const categoria = req.query.categoria;
   console.log(codice_fiscale,  team, categoria)
-  connection.query('SELECT * FROM erogazione, polo_per_categoria WHERE erogazione.polo_vaccinale = polo_per_categoria.polo_vaccinale AND categoria=\''+categoria+'\' ORDER BY erogazione.data, erogazione.ora', function (error, results, fields) {
-    // error will be an Error if one occurred during the query
-    // results will contain the results of the query
-    // fields will contain information about the returned results fields (if any)
+
+  connection.query('SELECT erogazioni_libere.* FROM polo_per_categoria, (SELECT erogazione.* from erogazione left join prenotazioni on erogazione.id_erogazione = prenotazioni.id_erogazione WHERE prenotazioni.id_erogazione IS NULL) as erogazioni_libere WHERE erogazioni_libere.polo_vaccinale = polo_per_categoria.polo_vaccinale AND categoria=\''+categoria+'\' ORDER BY erogazioni_libere.data, erogazioni_libere.ora', function (error, results, fields) {
+    console.log(results);
     res.render('poli-vaccinali', {title: 'Poli Vaccinali', 
                                   results: results, 
                                   identity:{"codice_fiscale": codice_fiscale, "team": team, "categoria":categoria}
